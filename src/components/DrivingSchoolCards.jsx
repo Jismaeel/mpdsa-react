a187b5b867bf469913d7fb7c77a41ec7742390f5
Jismaeel <146990352+Jismaeel@users.noreply.github.com>
2024-08-03 17:03:00 +0200

    changed links

--- src/components/DrivingSchoolCards.jsx
+++ src/components/DrivingSchoolCards.jsx
@@ -43,7 +43,7 @@ const DrivingSchoolCards = () => {
 
                 {/* <!-- Button --> */}
                 <a
-                  href="./SkylasDrivingSchool"
+                  href="/SkylasDrivingSchool"
                   className=" text-white  bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
                   Book now
--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -19,7 +19,7 @@ const Membership = () => {
               </p>
               <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
                 <a
-                  href="../member"
+                  href="/member"
                   className=" text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
                   Sign up
